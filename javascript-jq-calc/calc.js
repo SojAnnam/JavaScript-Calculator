bdb3ce7f533775e2e1484e9bcd702ae79f78688f
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-06-19 21:03:26 +0200

    fixed css

--- javascript-jq-calc/calc.js
+++ javascript-jq-calc/calc.js
@@ -1,15 +1,7 @@
 $(document).ready(function () {
-    var testNumLength = function (number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length - 9, 9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Error");
-            }
-        }
-    };
+
     var number = "";
-    var newnumber = "";
+    var numberOth = "";
     var operator = "";
     var totaldiv = $("#total");
     totaldiv.text("0");
@@ -20,7 +12,7 @@ $(document).ready(function () {
     });
     $("#operators a").not("#equals").click(function () {
         operator = $(this).text();
-        newnumber = number;
+        numberOthe = number;
         number = "";
         totaldiv.text(operator);
     });
@@ -28,62 +20,24 @@ $(document).ready(function () {
         number = "";
         totaldiv.text("0");
         if ($(this).attr("id") === "clearall") {
-            newnumber = "";
+            numberOther = "";
         }
     });
     $("#equals").click(function () {
         if (operator === "+") {
-            number = (parseInt(number, 10) + parseInt(newnumber, 10)).toString(10);
+            number = (parseInt(number, 10) + parseInt(numberOther, 10)).toString(10);
         } else if (operator === "-") {
-            number = (parseInt(newnumber, 10) - parseInt(number, 10)).toString(10);
+            number = (parseInt(numberOther, 10) - parseInt(number, 10)).toString(10);
         } else if (operator === "/") {
-            number = (parseInt(newnumber, 10) / parseInt(number, 10)).toString(10);
+            number = (parseInt(numberOther, 10) / parseInt(number, 10)).toString(10);
         } else if (operator === "*") {
-            number = (parseInt(newnumber, 10) * parseInt(number, 10)).toString(10);
+            number = (parseInt(numberOther, 10) * parseInt(number, 10)).toString(10);
         } else if (operator === "%") {
-            number = (parseInt(newnumber, 10) % parseInt(number, 10)).toString(10);
+            number = (parseInt(numberOther, 10) % parseInt(number, 10)).toString(10);
         }
         totaldiv.text(number);
-        testNumLength(number);
         number = "";
-        newnumber = "";
+        numberOthe = "";
     });
-    $(document).keypress(function (event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode === 49) {
-            $("#one").click();
-        } else if (keycode === 50) {
-            $("#two").click();
-        } else if (keycode === 51) {
-            $("#three").click();
-        } else if (keycode === 52) {
-            $("#four").click();
-        } else if (keycode === 53) {
-            $("#five").click();
-        } else if (keycode === 54) {
-            $("#six").click();
-        } else if (keycode === 55) {
-            $("#seven").click();
-        } else if (keycode === 56) {
-            $("#eight").click();
-        } else if (keycode === 57) {
-            $("#nine").click();
-        } else if (keycode === 48) {
-            $("#zero").click();
-        } else if (keycode === 97) {
-            $("#clearall").click();
-        } else if (keycode === 99) {
-            $("#clear").click();
-        } else if (keycode === 61) {
-            $("#equals").click();
-        } else if (keycode === 43) {
-            $("#plus").click();
-        } else if (keycode === 45) {
-            $("#minus").click();
-        } else if (keycode === 42 || keycode === 120) {
-            $("#multiply").click();
-        } else if (keycode === 47) {
-            $("#divide").click();
-        }
-    });
+
 });
